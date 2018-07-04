ce9b507bb005fa60e36afec81b57005253c5190c
Adrian García Acosta <elatry@hotmail.com>
2018-07-04 18:48:05 +0200

    Uso de servicios

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,17 +10,29 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
+    // this.getHeroes();
+    this.getHeroesObservable();
   }
 
   onSelect(hero: Hero): void {
+    this.messageService.add('HeroesComponent: selected heroe');
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  getHeroesObservable(): void {
+    this.heroService.getHeroesObservable()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
